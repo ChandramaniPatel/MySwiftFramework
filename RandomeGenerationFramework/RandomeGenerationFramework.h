8a6e38e5897e0f246285fb65ef3c38171354ecae
Chandramani <Chandramani1983@gmail.com>
2019-01-02 13:07:06 +0530

    Initial Commit

---
+++ RandomeGenerationFramework/RandomeGenerationFramework.h
@@ -0,0 +1,19 @@
+//
+//  RandomeGenerationFramework.h
+//  RandomeGenerationFramework
+//
+//  Created by Chandramani on 02/01/19.
+//  Copyright © 2019 Chandramani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RandomeGenerationFramework.
+FOUNDATION_EXPORT double RandomeGenerationFrameworkVersionNumber;
+
+//! Project version string for RandomeGenerationFramework.
+FOUNDATION_EXPORT const unsigned char RandomeGenerationFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RandomeGenerationFramework/PublicHeader.h>
+
+

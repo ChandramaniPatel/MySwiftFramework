b66d21d2b665cae4b4d443852a35cd9ab9cd9292
Chandramani <Chandramani1983@gmail.com>
2019-01-02 17:51:51 +0530

    "Sample swift framework"

---
+++ RandomeGenerationFramework/RandomGenerator.swift
@@ -0,0 +1,26 @@
+//
+//  RandomGenerator.swift
+//  RandomeGenerationFramework
+//
+//  Created by Chandramani on 02/01/19.
+//  Copyright © 2019 Chandramani. All rights reserved.
+//
+
+import Foundation
+
+// only visible to outside ,not for subclassing
+
+public class RandomGenerator {
+    
+    private init() {}
+    
+    public static func string()->String {
+        return UUID().uuidString
+    }
+    
+    public static func integer() -> Int {
+        return Int(arc4random())
+    }
+}
+
+
